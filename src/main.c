f5b1ed2faf007ac9c8ef12b2db2ea4bb725f19b0
John Hawthorn <jhawthor@uvic.ca>
2008-06-16 16:35:23 -0700

    replace popcount with ctz

--- src/main.c
+++ src/main.c
@@ -34,21 +34,17 @@ void usage(){
 	exit(1);
 }
 
-static int popcount(unsigned long mask){
-#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
-    return __builtin_popcount(mask);
-#else
-    int y;
-
-    y = (mask >> 1) & 033333333333;
-    y = mask - y - ((y >> 1) & 033333333333);
-    return (((y + (y >> 3)) & 030707070707) % 077);
-#endif
-}
-
-
 unsigned int getshift(unsigned int mask){
-	return popcount((mask - 1) & ~mask) & 31;
+#if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
+	return __builtin_ctz(mask);
+#else
+	int i = 0;
+	while((mask & 1) == 0){
+		i++;
+		mask >>= 1;
+	}
+	return i;
+#endif
 }
 
 void setaspect(int w, int h){
